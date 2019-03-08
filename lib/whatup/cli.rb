0d65a16d18614f7e9fff5bb60c7bbfb83b63e4c4
Mark Delk <jethrodaniel@gmail.com>
2019-03-08 00:24:27 -0600

    add client and server command skeleton

--- lib/whatup/cli.rb
+++ lib/whatup/cli.rb
@@ -2,10 +2,33 @@
 
 require 'thor'
 
-# Thor command class for the cli
-class CLI < Thor
-  desc 'hello', 'Says hello'
-  def hello
-    Thor::Shell::Basic.new.say "Hello!", :cyan
+module Whatup
+  class ClientCLI < Thor
+    desc 'start', 'Starts a client instance'
+    def start
+      say 'starting a client ... ', :green
+    end
+  end
+
+  class ServerCLI < Thor
+    desc 'start', 'Starts a server instance'
+    def start
+      say 'starting a server ... ', :green
+    end
+  end
+
+  # Thor command class for the cli.
+  # For usage, see <http://whatisthor.com/>
+  class CLI < Thor
+    desc 'hello', 'Says hello'
+    def hello
+      say "Hello!", :cyan
+    end
+
+    desc 'server ...', 'Perform server commands'
+    subcommand 'server', ServerCLI
+
+    desc 'client ...', 'Perform client commands'
+    subcommand 'client', ClientCLI
   end
 end
