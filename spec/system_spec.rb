b0a41b85e06f070946c440bc8d23d44a19cf7edc
Mark Delk <jethrodaniel@gmail.com>
2019-03-27 13:52:56 -0500

    make server restart for every spec

--- spec/system_spec.rb
+++ spec/system_spec.rb
@@ -7,23 +7,27 @@ require 'fileutils'
 def stop_server_and_remove_pid_file!
   system "kill #{$pid}" if $pid
   FileUtils.rm_f "#{Dir.home}/.whatup.pid"
+  sleep 0.5
+end
+
+def start_server!
+  $pid = spawn 'exe/whatup server start', out: '/dev/null', err: '/dev/null'
+  sleep 0.5
 end
 
 RSpec.describe 'whatup', type: :aruba do
   before(:each) do
+    stop_server_and_remove_pid_file!
+    start_server!
     run_command 'exe/whatup client connect', exit_timeout: 0.1
   end
 
-  before(:all) do
-    stop_server_and_remove_pid_file!
-    $pid = spawn 'exe/whatup server start', out: '/dev/null', err: '/dev/null'
-  end
-  after(:all) { stop_server_and_remove_pid_file! }
+  after(:each) { stop_server_and_remove_pid_file! }
 
   context 'when the server first starts up' do
     let(:pid_file) { "#{Dir.home}/.whatup.pid" }
     it "writes it's pid to a file" do
-      sleep 1
+      sleep 0.5
       expect(File.exist?(pid_file)).to be true
       expect(File.read(pid_file).chomp.to_i).to eq($pid)
     end
@@ -41,7 +45,31 @@ RSpec.describe 'whatup', type: :aruba do
 
     it 'requires a name' do
       type 'zeus'
-      sleep 1
+      sleep 0.5
+      expect(last_command_stopped.output).to eq output
+    end
+  end
+
+  context 'after connecting to the server' do
+    let(:output) do
+      <<~OUTPUT
+        Connecting to localhost:9001 ...
+        Please enter your username to establish a connection...
+        > > > Hello, zeus!
+        Commands:
+          chat            # starts a chat with the specified client
+          exit            # closes a client's connection with the server
+          help [COMMAND]  # Describe available commands or one specific command
+          list            # show all connected clients
+
+        Exiting ...
+      OUTPUT
+    end
+
+    it 'shows help if `help` is called' do
+      type 'zeus'
+      type 'help'
+      sleep 0.5
       expect(last_command_stopped.output).to eq output
     end
   end
